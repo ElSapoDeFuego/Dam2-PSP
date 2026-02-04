1d5f99f869fdc97c75b0e533395c41387137719d
ElSapoDeFuego <alejandro.iltd@gmail.com>
2026-02-04 19:47:27 +0100

    hecho 3.5 menudo crack que soy soy el udp

--- PSP/src/ejstcp/Actividad3_5/Cliente.java
+++ PSP/src/ejstcp/Actividad3_5/Cliente.java
@@ -1,10 +1,53 @@
 package ejstcp.Actividad3_5;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class Cliente {
-public static void main(String[] args) {
-	DatagramPacket envio = new DatagramPacket(mensaje, mensaje.length, destino, port);
-	DatagramSocket socket = new DatagramSocket(34567);//Puerto local
-	socket.send(envio);
-	socket.close();
-}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int puertoSalida = 50001;
+		int puertoentrada = 50000;
+		
+		String mensaje = "";
+		try (DatagramSocket socket = new DatagramSocket();) {
+			while (!mensaje.equals("*")) {
+				InetAddress destino = InetAddress.getLocalHost();
+
+				try {
+					
+					
+					System.out.println("Escribe lo que quieras enviar, si quieres parar pon *");
+					mensaje = sc.nextLine();
+					byte[] enviar = mensaje.getBytes();
+					DatagramPacket envio = new DatagramPacket(enviar, enviar.length, destino, puertoSalida);
+
+					socket.send(envio);
+					byte[] bufer = new byte[1024];
+					DatagramPacket recibo = new DatagramPacket(bufer, bufer.length);
+					socket.receive(recibo);
+					String reciboString = new String(recibo.getData(), 0, recibo.getLength());
+					System.out.println("Respuesta: "+ reciboString);
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SocketException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+	}
 }
--- PSP/src/ejstcp/Actividad3_5/Servidor.java
+++ PSP/src/ejstcp/Actividad3_5/Servidor.java
@@ -1,7 +1,42 @@
 package ejstcp.Actividad3_5;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+
 public class Servidor {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		int puertoEntrada = 50001;
+		int puertoRecibido;
+
+		byte[] mensajeRecibidoBytesParaDevolver;
+		String mensajeRecibido = "";
+
+		try (DatagramSocket socket = new DatagramSocket(puertoEntrada)) {
+			socket.setSoTimeout(50000);
+			System.out.println("Esperando a cliente");
+			while (!mensajeRecibido.trim().equals("*")) {
+
+				byte[] bufer = new byte[1024];
+				DatagramPacket recibo = new DatagramPacket(bufer, bufer.length);
+				socket.receive(recibo);
+				InetAddress IPOrigen = recibo.getAddress();
+				puertoRecibido = recibo.getPort();
+				mensajeRecibido = new String(recibo.getData(), 0, recibo.getLength()).toUpperCase();
+				mensajeRecibidoBytesParaDevolver = mensajeRecibido.getBytes();
+				System.out.println("Enviando: " + mensajeRecibido);
+				DatagramPacket envio = new DatagramPacket(mensajeRecibidoBytesParaDevolver,
+						mensajeRecibidoBytesParaDevolver.length, IPOrigen, puertoRecibido);
+				socket.send(envio);
+			}
+
+		} catch (SocketTimeoutException e) {
+			System.out.println("Tiempo agotado :");
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 }
